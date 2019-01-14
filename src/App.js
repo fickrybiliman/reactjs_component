319a7843ad09f7c4088057aaa64519aac599ef7f
fickrybiliman <bil.iman.fickry@gmail.com>
2019-01-14 17:26:41 +0700

    complete testing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Paragraph from './components/Paragraph';
+import Title from './components/Title';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      angka1: 0,
+      angka2: 0,
+      hasil: 0
+    }
+  }
+
+  _handleClick = () => {
+    const perhitungan = Number(this.state.angka1) + Number(this.state.angka2)
+    this.setState({hasil: perhitungan})
+  }
+
+  _handleChangeAngka1 = (element) => {
+    this.setState({ angka1: element.target.value })
+  }
+
+  _handleChangeAngka2 = (element) => {
+    this.setState({ angka2: element.target.value })
+  }
+
+  _handleChange = (element) => {
+    this.setState({ [element.target.name]: element.target.value })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Title/>
+        <Paragraph></Paragraph>
+        <input 
+          name = 'angka1'
+          onChange = {this._handleChange}
+          value = { this.state.angka1 } 
+        />
+        <input 
+          name = 'angka2'
+          onChange = { this._handleChange}
+          value = { this.state.angka2 }
+        />
+        <button onClick = {this._handleClick } >HITUNG</button>
+        <h1>Hasil : { this.state.hasil }</h1>
       </div>
     );
   }
---
+++ src/components/Paragraph.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Paragraph extends Component {
+   render() {
+      return <p>Lorem Ipsum dari Component Lain</p>
+   }
+}
+
+export default Paragraph;
